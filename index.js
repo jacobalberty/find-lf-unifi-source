947a7584354fae46956eccf06105f4e651aa3a7d
Jacob Alberty <jacob.alberty@vetmanage.com>
2018-01-31 13:10:31 -0600

    Close out on any error from the request and only output when a new ap sends data from the controller.

--- index.js
+++ index.js
@@ -22,14 +22,25 @@ var statOptions = {
     uri: `${config.unifi.url}/api/s/${config.unifi.site}/stat/sta`
 }
 
+var nodes = { };
+
 function getStats() {
     request(Object.assign({}, baseOptions, statOptions), function(error, response, body) {
+        if (error) {
+            console.log(`error: ${error}`);
+            clearInterval(iVar);
+            return;
+        }
         var data = JSON.parse(body).data;
         var payloads = { };
         var timestamp = Date.now();
         data.forEach(function(client) {
             if (!client.is_wired) {
                 if (payloads[client.ap_mac] == undefined) {
+                    if (nodes[client.ap_mac] == undefined) {
+                        console.log(`new node '${client.ap_mac}' online at ${timestamp}`);
+                        nodes[client.ap_mac] = timestamp;
+                    }
                     payloads[client.ap_mac] = {
                         node: client.ap_mac,
                         signals: [ ],
@@ -55,15 +66,18 @@ function getStats() {
                     if (error) {
                         console.log(`error: ${error}`);
                         clearInterval(iVar);
+                        return;
                     }
-                    console.log(`sent data: ${node}`);
-
                 });
         });
     });
 }
 
 request(Object.assign({}, baseOptions, loginOptions), function(error, response, body) {
+    if (error) {
+        console.log(`error: ${error}`);
+        return;
+    }
     getStats();
     iVar = setInterval(getStats, config.other.interval*1000);
 });
